f62c17cc0ab7112a3b2af1b703dfd4cb3ff07e33
Rishi <61271406+rishi7pillai@users.noreply.github.com>
2021-03-19 20:16:11 +0530

    Create AllpythonProgm.cpp

---
+++ PythonProgramming/AllpythonProgm.cpp
@@ -0,0 +1,48 @@
+ monkey patching python
+----------------------------
+
+class Test:
+   def __init__(self,x):
+        self.a=x
+   def getdata(self):
+       print("some code to fetch the data from data base")
+   def f1(self):
+       self.getdata()
+   def f2(self):
+       self.getdata()
+        
+
+x=Test(5)
+x.f1()
+x.f2()
+def get_update():
+    print("some code")
+
+x.f1=get_update
+x.f1()
+x.f2()
+
+
+=================================================================
+Produce first N prime number using generator
+
+
+def primeGenerator(n):
+    num=2
+    while n:
+        if isPrime(num):
+            yield num
+            n-=1
+        num+=1  
+    return
+
+def isPrime(num):
+    for i in range(2,num-1):
+        if num%i==0:
+            return False
+    return True
+        
+it =primeGenerator(10)
+for i in it:
+    print(i,end=' ')
+
